07236f5a4cc33596705bcadc73444e2bcfbccac9
darkwizz <psyhoge@gmail.com>
2015-09-12 17:10:28 +0300

    add build, start and stop

--- .openshift/action_hooks/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $OPENSHIFT_REPO_DIR
-
-if [[ -z "$OPENSHIFT_INTERNAL_IP" ]]; then export OPENSHIFT_INTERNAL_IP="$OPENSHIFT_DIY_IP"; fi
-if [[ -z "$OPENSHIFT_INTERNAL_PORT" ]]; then export OPENSHIFT_INTERNAL_PORT="$OPENSHIFT_DIY_PORT"; fi
-
-nohup java -jar target/httpnettyserver-1.0-SNAPSHOT.jar > ${OPENSHIFT_DIY_LOG_DIR}/trynetty.log 2 > &1 &
--- .openshift/action_hooks/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ -z "$OPENSHIFT_INTERNAL_IP" ]]; then export OPENSHIFT_INTERNAL_IP="$OPENSHIFT_DIY_IP"; fi
-if [[ -z "$OPENSHIFT_INTERNAL_PORT" ]]; then export OPENSHIFT_INTERNAL_PORT="$OPENSHIFT_DIY_PORT"; fi
-
-wget http://${OPENSHIFT_INTERNAL_IP}/stop
